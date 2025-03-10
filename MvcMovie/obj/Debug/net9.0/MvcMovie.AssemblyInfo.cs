c898a691273e754e164d8f1167949c1dda4d59ed
datsang03 <sangnguyen.04121999@gmail.com>
2025-03-10 14:20:04 +0700

    crud

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b15b1ff8b10f06b2f2b1ab101b2a2b57bcc8af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d02e49a5a812d664c83a9bfb933701d75d77fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
